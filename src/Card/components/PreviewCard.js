fde56faad5fea39124487c6b3eed56abcf2c9ae1
brandonn <brandon7.7porcel@gmail.com>
2021-06-01 17:42:30 -0300

    los styles min 768px ya estan.termine

--- src/Card/components/PreviewCard.js
+++ src/Card/components/PreviewCard.js
@@ -16,67 +16,65 @@ export default function PreviewCard({ user, setUser, initialUserInfo }) {
 			: d.querySelector('.preview__card--image').classList.remove('big');
 	}, [user.instagram]);
 	return (
-		<section>
-			<div
-				id="preview-card-ctn"
-				className={`preview__card preview-${
-					user.palette === '1'
-						? 'default'
-						: user.palette === '2'
-						? 'second'
-						: user.palette === '3'
-						? 'third'
-						: ''
-				}`}
-			>
-				<button className="reset-btn" type="reset" onClick={handleReset}>
-					<FontAwesomeIcon
-						className="reset-icon"
-						icon={faRecycle}
-					></FontAwesomeIcon>
-					reset
-				</button>
-				<article className="pantone-container" id="pantone-container">
-					<div
-						id="preview-card-img"
-						className="preview__card--image"
-						// style={{  backgroundImage: `url(${user.foto})` }}
-						style={{
-							backgroundImage: `url(${
-								user.foto === '' ? initialUserInfo.foto : user.foto
-							})`,
-						}}
-					/>
-					<div className="preview__card--text">
-						<h2 className="card-title">
-							{user.nombre === '' ? initialUserInfo.nombre : user.nombre}
-						</h2>
-						<h3 className="card-subtitle">
-							{user.puesto === '' ? initialUserInfo.puesto : user.puesto}
-						</h3>
-					</div>
+		<section
+			id="preview-card-ctn"
+			className={`preview__card preview-${
+				user.palette === '1'
+					? 'default'
+					: user.palette === '2'
+					? 'second'
+					: user.palette === '3'
+					? 'third'
+					: ''
+			}`}
+		>
+			<button className="reset-btn" type="reset" onClick={handleReset}>
+				<FontAwesomeIcon
+					className="reset-icon"
+					icon={faRecycle}
+				></FontAwesomeIcon>
+				reset
+			</button>
+			<article className="pantone-container" id="pantone-container">
+				<div
+					id="preview-card-img"
+					className="preview__card--image"
+					// style={{  backgroundImage: `url(${user.foto})` }}
+					style={{
+						backgroundImage: `url(${
+							user.foto === '' ? initialUserInfo.foto : user.foto
+						})`,
+					}}
+				/>
+				<div className="preview__card--text">
+					<h2 className="card-title">
+						{user.nombre === '' ? initialUserInfo.nombre : user.nombre}
+					</h2>
+					<h3 className="card-subtitle">
+						{user.puesto === '' ? initialUserInfo.puesto : user.puesto}
+					</h3>
+				</div>
 
-					<nav
-						className={user.instagram === '' ? 'hide' : 'preview__card--social'}
-					>
-						<ul className="preview__card--ul">
-							<li className="social-card-li">
-								<a
-									className="social-card-link"
-									target="_blank"
-									rel="noreferrer"
-									href={`https://instagram.com/${user.instagram}`}
-								>
-									<FontAwesomeIcon
-										className="social-card-icon"
-										icon={faInstagram}
-									></FontAwesomeIcon>
-								</a>
-							</li>
-						</ul>
-					</nav>
-				</article>
-			</div>
+				<nav
+					className={user.instagram === '' ? 'hide' : 'preview__card--social'}
+				>
+					<ul className="preview__card--ul">
+						<li className="social-card-li">
+							<a
+								className="social-card-link"
+								target="_blank"
+								rel="noreferrer"
+								href={`https://instagram.com/${user.instagram}`}
+							>
+								<FontAwesomeIcon
+									className="social-card-icon"
+									icon={faInstagram}
+								></FontAwesomeIcon>
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</article>
 		</section>
 	);
 }
